8bc1cc8c58efb4ed89095fd17b1c95bda0276be3
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-10 08:03:06 +0900

    [update] 文字と文字列の基本的な型。

--- Main.cpp
+++ Main.cpp
@@ -6,11 +6,17 @@ void Main()
 	int32  b = 123;
 	double c = 0.5;
 	size_t d = 100;
+	char32 e = U'A';
+	String f = U"Hello";
+	FilePath g = U"example/windmill.png";
 
 	Print << U"a:" << a;
 	Print << U"b:" << b;
 	Print << U"c:" << c;
 	Print << U"d:" << d;
+	Print << U"e:" << e;
+	Print << U"f:" << f;
+	Print << U"g:" << g;
 
 	while (System::Update())
 	{
