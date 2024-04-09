fc38ecf9cca175a8e379bc05d30ca5ac114d8461
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-10 07:44:23 +0900

    [update] 簡易的なデータ表示

--- Main.cpp
+++ Main.cpp
@@ -2,32 +2,16 @@
 
 void Main()
 {
-	ScreenCapture::SetShortcutKeys({ KeyA });
+	Print << U"こんにちは";
 
-	LicenseManager::AddLicense({
-		.title = U"My game",
-		.copyright = U"(C) 2023 My name",
-		.text = U"License" });
-
-	// System::SetTerminationTriggers(UserAction::CloseButtonClicked);
-
-	// 終了操作を設定しない
-	System::SetTerminationTriggers(UserAction::NoAction);
-
-	// 終了操作をデフォルトに戻す
-	System::SetTerminationTriggers(UserAction::Default);
-
-	// 画像ファイルから画像データを読み込んでテクスチャを作成する
-	const Texture texture{ U"example/windmill.png" };
+	int32 count = 0;
 
 	while (System::Update())
 	{
-		// テクスチャを描画する
-		texture.draw();
+		ClearPrint();
 
-		if (5.0 <= Scene::Time())
-		{
-			System::Exit();
-		}
+		Print << count;
+
+		count++;
 	}
 }
