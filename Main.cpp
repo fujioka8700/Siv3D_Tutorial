861b959ea4550a03bfdc67e3e4937da3fc773dcc
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-09 17:06:01 +0900

    [update] メインループの周期

--- Main.cpp
+++ Main.cpp
@@ -4,16 +4,8 @@ void Main()
 {
 	while (System::Update())
 	{
-		Print << Scene::Time();
+		int32 fps = Profiler::FPS();
 
-		// 実行時間が 3 秒以上経過したら
-		if (3.0 <= Scene::Time())
-		{
-			return;
-		}
-
-		// 処理A();
+		Window::SetTitle(fps);
 	}
-
-	// 処理B();
-} // ここで Main() が終了
+}
