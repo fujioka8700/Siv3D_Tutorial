63d7e0945e5068f4824a7ce0c2f54d2eb2d406f9
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-10 07:24:46 +0900

    [update] 終了操作を、ウィンドウを閉じるのみに変更。

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,13 @@ void Main()
 {
 	ScreenCapture::SetShortcutKeys({ KeyA });
 
+	LicenseManager::AddLicense({
+		.title = U"My game",
+		.copyright = U"(C) 2023 My name",
+		.text = U"License" });
+
+	System::SetTerminationTriggers(UserAction::CloseButtonClicked);
+
 	// 画像ファイルから画像データを読み込んでテクスチャを作成する
 	const Texture texture{ U"example/windmill.png" };
 
