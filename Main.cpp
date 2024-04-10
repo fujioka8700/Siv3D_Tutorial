6a3e9fa64789da5f73db17e8627efed4a6c115c2
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-10 09:59:32 +0900

    [update] 背景を白色にする

--- Main.cpp
+++ Main.cpp
@@ -2,22 +2,10 @@
 
 void Main()
 {
-	Array<int32> a = {10, 20, 50, 100};
-	Optional<double> b;
-
-	Print << U"a:" << a;
-	Print << U"b:" << b;
-
-	b = 12.3;
-
-	Print << U"b:" << b;
-
-	b = none;
-
-	Print << U"b:" << b;
+	Scene::SetBackground(Palette::White);
 
 	while (System::Update())
 	{
-		
+
 	}
 }
