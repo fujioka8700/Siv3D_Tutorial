6e567f1db3ec64ad867298e9b0500d69966e865b
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-11 20:52:26 +0900

    [update] 半透明の色を指定する。

--- Main.cpp
+++ Main.cpp
@@ -4,12 +4,12 @@ void Main()
 {
 	while (System::Update())
 	{
-		Circle{ 100, 200, 40}.draw();
-		Circle{ 200, 200, 40}.draw(Palette::Green);
-		Circle{ 300, 200, 40}.draw(Palette::Skyblue);
-		Circle{ 400, 200, 40 }.draw(ColorF{ 1.0, 0.8, 0.0 });
-		Circle{ 500, 200, 40 }.draw(ColorF{ 0.8 });
-		Circle{ 600, 200, 40 }.draw(HSV{ 160.0, 0.5, 1.0 });
-		Circle{ 700, 200, 40 }.draw(HSV{ 160.0 });
+		Circle{ 400, 300, 200}.draw();
+
+		Circle{ 200, 300, 200 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.9 });
+
+		Circle{ 600, 300, 200 }.draw(HSV{ 240.0, 0.5, 1.0, 0.2 });
+
+		Circle{ Cursor::Pos(), 100 }.draw(ColorF{ 0.0, 0.5 });
 	}
 }
