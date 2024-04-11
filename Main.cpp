6af99779682ba145edfa3663445c642af9b0f046
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-11 19:30:36 +0900

    [update] 背景色を RGB で指定する。

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,16 @@
 ﻿# include <Siv3D.hpp>
 
+struct X {
+	X(int a) {
+		Print << U"aaa" << a;
+	}
+};
+
 void Main()
 {
-	Scene::SetBackground(Palette::Aliceblue);
+	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+
+	X x1{0};
 
 	while (System::Update())
 	{
