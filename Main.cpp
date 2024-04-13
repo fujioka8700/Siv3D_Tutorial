36bf9999eb4ed97b139140425bd04bffaadb6ffb
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-13 20:04:04 +0900

    [update] グラデーションで長方形を描く。その2。

--- Main.cpp
+++ Main.cpp
@@ -7,14 +7,6 @@ void Main()
 	while (System::Update())
 	{
 		Rect{ 0, 0, 600, 500 }
-			.draw(Arg::top = ColorF{ 0.5, 0.7, 0.9 }, Arg::bottom = ColorF{ 0.5, 0.9, 0.7 });
-
-		Rect{ 220, 100, 100, 30 }.drawFrame(3, 0);
-
-		Rect{ 340, 100, 100, 30 }.drawFrame(0, 3);
-
-		Rect{ 200, 200, 400, 100 }.drawFrame(3, 3, Palette::Orange);
-
-		Circle{ Cursor::Pos(), 40 }.drawFrame(1, 1, Palette::Seagreen);
+			.draw(Arg::left = ColorF{ 0.5, 0.7, 0.9 }, Arg::right = ColorF{ 0.5, 0.9, 0.7 });
 	}
 }
