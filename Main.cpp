de30a6972964b207af595e217a43d452048a36fd
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-14 17:58:44 +0900

    [update] 円を並べる

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,18 @@
 
 void Main()
 {
-	Scene::SetBackground(ColorF{ 0.3, 0.6, 1.0 });
+	Scene::SetBackground(Palette::White);
 
 	while (System::Update())
 	{
-		Rect{ 0, 0, 600, 500 }
-			.draw(Arg::left = ColorF{ 0.5, 0.7, 0.9 }, Arg::right = ColorF{ 0.5, 0.9, 0.7 });
+		for (int32 i = 0; i < 5; i++)
+		{
+			Circle{ (i * 100), 100, 30 }.draw(Palette::Skyblue);
+		}
+
+		for (int32 i = 0; i < 5; i++)
+		{
+			Circle{ (50 + i * 100), 200, 30 }.draw(Palette::Seagreen);
+		}
 	}
 }
