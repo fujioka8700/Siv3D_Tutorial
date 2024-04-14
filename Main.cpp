71f07fd70e7be5eeda17ceb3a5c3caa0d7084b96
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-14 18:13:26 +0900

    [update] 長方形を並べる

--- Main.cpp
+++ Main.cpp
@@ -6,19 +6,9 @@ void Main()
 
 	while (System::Update())
 	{
-		for (int32 y = 0; y < 5; y++)
+		for (int32 x = 0; x < 6; x++)
 		{
-			for (int32 x = 0; x < 6; x++)
-			{
-				if ((x + y) % 2 == 0)
-				{
-					Circle{ (x * 100), (y * 100), 10 }.draw(Palette::Skyblue);
-				}
-				else
-				{
-					Circle{ (x * 100), (y * 100), 30 }.draw(Palette::Skyblue);
-				}
-			}				
+			Rect{ (x * 100), 0, 80, 600 }.draw(Palette::Skyblue);
 		}
 	}
 }
